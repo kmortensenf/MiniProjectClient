54f9fed5c519579366768c09aa72fa7ef5a64942
Delan88 <dali17@student.aau.dk>
2018-10-10 16:07:58 +0200

    addede RunningFame Overload for at lave GUI og knapper samt et Tekst område bare for test alt andet er stadig beholdt som kommentar vis det stadig skal bruges

--- src/RunningGame.java
+++ src/RunningGame.java
@@ -1,12 +1,29 @@
 import javax.swing.*;
+
+import L21.ListenForKeys;
+import L21.ListenForWindow;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 import javax.swing.*;
-public class RunningGame {
+import java.awt.Toolkit;
+import java.awt.Dimension;
 
+public class RunningGame extends JFrame
+{
+		JButton Stand, Hit, Bet;
+		JTextArea Area;
+		JTextField txtF1;
 
-    public static void createAndShowGUI(){
+    public static void createAndShowGUI()
+    {
+    	/*
         JFrame GameWindow = new JFrame("Black Jack");
         GameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JLabel emptyLabel = new JLabel("");
@@ -16,10 +33,77 @@ public class RunningGame {
         GameWindow.getContentPane().add(emptyLabel, BorderLayout.CENTER);
         GameWindow.pack();
         GameWindow.setVisible(true);
-
+    	 */
+    	new RunningGame();
     }
+    
+    public RunningGame()
+    {
+    	Toolkit t = Toolkit.getDefaultToolkit();
+		 
+		
+		Dimension d = t.getScreenSize();
+		this.setSize(d.width / 4, d.height / 2);
+		int xPos = (d.width / 2) - (this.getWidth() / 2);
+		int yPos = (d.height / 2) - (this.getHeight() / 2);
+		
+		this.setLocation(xPos, yPos);
+		
+		//this.setResizable(false);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Test Frame");
+		
+		JPanel Frame = new JPanel();
+		
+		Area = new JTextArea(15, 40);
+		Area.setLineWrap(true);
+		Area.setWrapStyleWord(true);
+		JScrollPane Scroll = new JScrollPane(Area, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		Frame.add(Scroll);
+		
+		Stand = new JButton("Stand");
+		Bet = new JButton("Bet");
+		Hit = new JButton("Hit");
+		ListenForButton lFB = new ListenForButton();
+		Stand.addActionListener(lFB);
+		Bet.addActionListener(lFB);
+		Hit.addActionListener(lFB);
+		Frame.add(Stand);
+		Frame.add(Bet);
+		Frame.add(Hit);
+		
+		this.add(Frame);
+		
+		this.setVisible(true);
+		
+    }
+    
+    	private class ListenForButton implements ActionListener
+    	{
 
-    public static void main (String[] args){
+			public void actionPerformed(ActionEvent e) 
+			{
+				if(e.getSource() == Stand)
+				{
+					Area.append("Tets for Stand");
+					Area.append("\n");
+				} else if (e.getSource() == Hit)
+				{
+					Area.append("Tets for Hit");
+					Area.append("\n");
+				} else if (e.getSource() == Bet)
+				{
+					Area.append("Tets for Bet");
+					Area.append("\n");
+				}
+				
+			}
+    		
+    	}
+	
+	public static void main (String[] args){
 
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
